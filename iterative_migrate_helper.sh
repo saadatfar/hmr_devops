a0f1cf162bedb92240cb9af54735ca08aff26226
Mohammad Saadatfar <mh.saadatfar@gmail.com>
2024-06-28 15:12:50 -0700

    Update iterative_migrate_helper.sh

--- iterative_migrate_helper.sh
+++ iterative_migrate_helper.sh
@@ -9,11 +9,20 @@ fi
 TOKEN=$1
 DRY_RUN=$2
 
+# Function to execute the curl command
+execute_curl() {
+  local dir=$1
+  echo -n "$dir --> "
+  result=$(cd "$dir" && curl -s https://raw.githubusercontent.com/saadatfar/hmr_devops/main/migrate_helper.sh?token=$(date +%s) | bash -s "$TOKEN" $DRY_RUN)
+  echo "$result"
+}
+
+# Run on the current directory
+execute_curl "."
+
 # Iterate over each subfolder in the current directory
 for dir in */; do
   if [ -d "$dir" ]; then
-    echo -n "$dir --> "
-    result=$(cd "$dir" && curl -s https://raw.githubusercontent.com/saadatfar/hmr_devops/main/migrate_helper.sh?token=$(date +%s) | bash -s "$TOKEN" $DRY_RUN)
-    echo "$result"
+    execute_curl "$dir"
   fi
 done
